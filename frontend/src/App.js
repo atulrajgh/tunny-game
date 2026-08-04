a7546f0c5560272ec288b1379a01b6d9817ab167
atulrajgh <atul.r.a.j@gmail.com>
2026-08-03 22:46:04 -0500

    Move How to Play link below scoreboard for everyone; drop admin panel duplicate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -538,7 +538,6 @@ function App() {
                 </button>
               )}
           </div>
-          <a href="/instructions" target="_blank" className="help-link">How to Play</a>
         </div>
       </div>
 
@@ -783,6 +782,7 @@ function App() {
         <div className="ts-row header"><span></span><span>Score</span><span>HCP</span></div>
         <div className="ts-row ns"><span>N-S</span><span>{gameState.scores?.['N-S'] || 0}</span><span>{gameState.teamPoints?.['N-S'] || 0}</span></div>
         <div className="ts-row ew"><span>E-W</span><span>{gameState.scores?.['E-W'] || 0}</span><span>{gameState.teamPoints?.['E-W'] || 0}</span></div>
+        <a href="/instructions" target="_blank" className="help-link">How to Play</a>
       </div>
 
       {/* Action buttons */}
